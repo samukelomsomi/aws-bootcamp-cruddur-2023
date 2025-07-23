1b08b3fc60131e0f156ad352aaba953d90b7ef60
samukelo msomi <samkelomsomi@outlook.com>
2025-07-23 19:30:07 +0000

    implement clouldwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,19 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
 from services.home_activities import *
 from services.user_activities import *
 from services.create_activity import *
@@ -51,6 +64,12 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   user_handle  = 'andrewbrown'
@@ -88,7 +107,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
